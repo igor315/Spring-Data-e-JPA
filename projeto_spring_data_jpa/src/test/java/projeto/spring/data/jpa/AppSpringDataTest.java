f8ffbe4f49393f2471b6cf8bef81a85142485945
Igor Santos Oliveira <iigor315@hotmail.com>
2022-10-03 17:30:17 -0300

    Realizando cadastro no banco de dados

--- projeto_spring_data_jpa/src/test/java/projeto/spring/data/jpa/AppSpringDataTest.java
+++ projeto_spring_data_jpa/src/test/java/projeto/spring/data/jpa/AppSpringDataTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import projeto.spring.data.jpa.dao.InterfaceSpringDataUser;
+import projeto.spring.data.jpa.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
@@ -17,9 +18,26 @@ public class AppSpringDataTest {
 	
 	@Test
 	public void testeInsert() {
+		
+		/*Insancia um novo usuario e seta os dados*/
+		UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
+		usuarioSpringData.setEmail("Alexsotwaredeveloper@.com");
+		usuarioSpringData.setLogin("developer 123");
+		usuarioSpringData.setSenha("321");
+		usuarioSpringData.setNome("Alex Fernando");
+		
+		/*Salvamos todos os dados*/
+		interfaceSpringDataUser.save(usuarioSpringData);
+		
+		
+		/*Conta quantos usuarios estão cadastrados no banco*/
+		System.out.println("Usuarios Cadastrados " + interfaceSpringDataUser.count());
+	}
+	
+	@Test
+	public void testeConsulta() {
 		System.out.println("Iniciou spring com sucesso!");
 		// Amigo, problema resolvido! :)
 	}
-	
 
 }
