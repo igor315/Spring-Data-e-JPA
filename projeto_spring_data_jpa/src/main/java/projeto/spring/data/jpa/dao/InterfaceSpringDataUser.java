318f2a4ac331245fa5edbe51ce093b870e296a7b
Igor Santos Oliveira <iigor315@hotmail.com>
2022-10-04 17:49:35 -0300

    Realizando consultas customizadas com @Query

--- projeto_spring_data_jpa/src/main/java/projeto/spring/data/jpa/dao/InterfaceSpringDataUser.java
+++ projeto_spring_data_jpa/src/main/java/projeto/spring/data/jpa/dao/InterfaceSpringDataUser.java
@@ -1,5 +1,8 @@
 package projeto.spring.data.jpa.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +11,6 @@ import projeto.spring.data.jpa.model.UsuarioSpringData;
 @Repository
 public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringData, Long> {
 
-	
-	
+	@Query(value = "select p from UsuarioSpringData p where p.nome like %?1%")
+	public List<UsuarioSpringData> buscaPorNome(String nome);
 }
--- projeto_spring_data_jpa/src/test/java/projeto/spring/data/jpa/AppSpringDataTest.java
+++ projeto_spring_data_jpa/src/test/java/projeto/spring/data/jpa/AppSpringDataTest.java
@@ -1,5 +1,6 @@
 package projeto.spring.data.jpa;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
@@ -17,44 +18,43 @@ public class AppSpringDataTest {
 
 	@Autowired
 	private InterfaceSpringDataUser interfaceSpringDataUser;
-	
+
 	@Test
 	public void testeInsert() {
-		
-		/*Insancia um novo usuario e seta os dados*/
+
+		/* Insancia um novo usuario e seta os dados */
 		UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
 		usuarioSpringData.setEmail("Alexsotwaredeveloper@.com");
 		usuarioSpringData.setLogin("developer 123");
 		usuarioSpringData.setSenha("321");
 		usuarioSpringData.setNome("Alex Fernando");
-		
-		/*Salvamos todos os dados*/
+
+		/* Salvamos todos os dados */
 		interfaceSpringDataUser.save(usuarioSpringData);
-		
-		
-		/*Conta quantos usuarios estão cadastrados no banco*/
+
+		/* Conta quantos usuarios estão cadastrados no banco */
 		System.out.println("Usuarios Cadastrados " + interfaceSpringDataUser.count());
 	}
-	
+
 	@Test
 	public void testeConsulta() {
-		
+
 		Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(3L);
-		
+
 		System.out.println(usuarioSpringData.get().getId());
 		System.out.println(usuarioSpringData.get().getEmail());
 		System.out.println(usuarioSpringData.get().getNome());
 		System.out.println(usuarioSpringData.get().getLogin());
 		System.out.println(usuarioSpringData.get().getSenha());
 	}
-	
+
 	@Test
 	public void testeConsultaTodos() {
-		
+
 		Iterable<UsuarioSpringData> listausuarios = interfaceSpringDataUser.findAll();
-		
-		for(UsuarioSpringData usuarioSpringData : listausuarios) {
-			
+
+		for (UsuarioSpringData usuarioSpringData : listausuarios) {
+
 			System.out.println(usuarioSpringData.getId());
 			System.out.println(usuarioSpringData.getEmail());
 			System.out.println(usuarioSpringData.getNome());
@@ -62,26 +62,42 @@ public class AppSpringDataTest {
 			System.out.println(usuarioSpringData.getSenha());
 		}
 	}
-	
+
 	@Test
-	public void testeUpdate () {
-		
+	public void testeUpdate() {
+
 		Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(3L);
-		
+
 		UsuarioSpringData data = usuarioSpringData.get();
 		data.setSenha("@martins257");
-		
+
 		interfaceSpringDataUser.save(data);
 	}
-	
+
 	@Test
 	public void testeDelete() {
-		
-		/*pode passar diretamente pelo o id*/
-		/*interfaceSpringDataUser.deleteById(7L);*/
-		
+
+		/* pode passar diretamente pelo o id */
+		/* interfaceSpringDataUser.deleteById(7L); */
+
 		Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(4L);
 		interfaceSpringDataUser.delete(usuarioSpringData.get());
+	}
+
+	@Test
+	public void testeConsultaNome() {
+
+		List<UsuarioSpringData> list = interfaceSpringDataUser.buscaPorNome("Igor");
+
+		for (UsuarioSpringData usuarioSpringData : list) {
+
+			System.out.println(usuarioSpringData.getId());
+			System.out.println(usuarioSpringData.getEmail());
+			System.out.println(usuarioSpringData.getNome());
+			System.out.println(usuarioSpringData.getLogin());
+			System.out.println(usuarioSpringData.getSenha());
+		}
+
 	}
 
 }
